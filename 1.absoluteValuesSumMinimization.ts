12345f8c42c5096311231826a38c0276c036ae9e
Chris Azuaje <christopherazuaje@gmail.com>
2023-05-06 10:20:38 -0400

    Updated files

--- 1.absoluteValuesSumMinimization.ts
+++ 1.absoluteValuesSumMinimization.ts
@@ -1,12 +1,26 @@
 // Finding the median value (ultiamtely)
 
-function absoluteValueSumMinimization(a: number[]): number {
-  const isEven = a.length % 2 === 0;
+// function absoluteValueSumMinimization(a: number[]): number {
+//   const isEven = a.length % 2 === 0;
 
+//   return isEven ? a[a.length / 2 - 1] : a[Math.floor(a.length / 2)];
+// }
+
+// console.log(absoluteValueSumMinimization([2, 4, 7]));
+// console.log(absoluteValueSumMinimization([2, 4, 7, 6]));
+// console.log(absoluteValueSumMinimization([2, 4, 7, 6, 6]));
+// console.log(absoluteValueSumMinimization([2, 4, 7, 6, 6, 8]));
+
+//////////////////////////////////////////////////////////////////////////
+
+// Whiteboard Practice
+
+export function meanMinimization(a: number[]): number {
+  const isEven = a.length % 2 === 0;
   return isEven ? a[a.length / 2 - 1] : a[Math.floor(a.length / 2)];
 }
 
-console.log(absoluteValueSumMinimization([2, 4, 7]));
-console.log(absoluteValueSumMinimization([2, 4, 7, 6]));
-console.log(absoluteValueSumMinimization([2, 4, 7, 6, 6]));
-console.log(absoluteValueSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(meanMinimization([2, 4, 7]));
+console.log(meanMinimization([2, 4, 7, 6]));
+console.log(meanMinimization([2, 4, 7, 6, 6]));
+console.log(meanMinimization([2, 4, 7, 6, 6, 8]));
--- 2.sum.ts
+++ 2.sum.ts
@@ -9,14 +9,24 @@
 // --------------------------------------------
 
 // Add values of an array of any length
-function add2(...param1: number[]): number {
-  let total = 0;
+// function add2(...param1: number[]): number {
+//   let total = 0;
 
-  param1.forEach((num) => {
-    total += num;
-  });
-  return total;
+//   param1.forEach((num) => {
+//     total += num;
+//   });
+//   return total;
+// }
+
+// console.log(add2(1, 2, 3, 4, 5));
+// console.log(add2(2, 3));
+
+function add(...param: number[]): number {
+  let sum = 0;
+  for (let i = 0; i < param.length; i++) {
+    sum += param[i];
+  }
+  return sum;
 }
 
-console.log(add2(1, 2, 3, 4, 5));
-console.log(add2(2, 3));
+console.log(add(1, 2, 3, 80));
