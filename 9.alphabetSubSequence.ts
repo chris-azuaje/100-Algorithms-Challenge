4510ed115eda7917930f0e2ea46bbda8789cbd22
Chris Azuaje <christopherazuaje@gmail.com>
2023-05-09 16:05:55 -0400

    Committing the 9th exercise

---
+++ 9.alphabetSubSequence.ts
@@ -0,0 +1,27 @@
+// Check whether the given string is a subsequence of the plaintext alphabet.
+
+function alphabetSubsequence(s: string): boolean {
+  const chars: string[] = s.split("");
+  const charValues: number[] = [];
+
+  chars.forEach((char: string) => {
+    charValues.push(char.charCodeAt(0));
+  });
+
+  if (new Set(charValues).size !== charValues.length) {
+    return false;
+  }
+
+  for (let i = 0; i < charValues.length; i++) {
+    if (charValues[i] >= charValues[i + 1]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(alphabetSubsequence("effg"));
+console.log(alphabetSubsequence("cdce"));
+console.log(alphabetSubsequence("ace"));
+console.log(alphabetSubsequence("bxz"));
