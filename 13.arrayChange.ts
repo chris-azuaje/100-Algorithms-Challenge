98c925101a1e7ea159ff1645bbbc61b3ac0e8ec1
Chris Azuaje <christopherazuaje@gmail.com>
2023-05-19 07:35:18 -0400

    Committing the 13th exercise

---
+++ 13.arrayChange.ts
@@ -0,0 +1,15 @@
+// You are given an array of integers. On each move you are allowed to increase exactly one of its element by one. Find the minimal number of moves required to obtain a strictly increasing sequence from the input.
+
+function arrayChange(inputArray: number[]): number {
+  let count = 0;
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i] >= inputArray[i + 1]) {
+      const difference = inputArray[i] + 1 - inputArray[i + 1];
+      inputArray[i + 1] = inputArray[i] + 1;
+      count += difference;
+    }
+  }
+  return count;
+}
+
+console.log(arrayChange([1, 1, 1]));
