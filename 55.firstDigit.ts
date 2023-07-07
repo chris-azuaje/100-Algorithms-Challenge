c40ea2d8c2fb105c3879c1914bda08625aecd99b
Chris Azuaje <christopherazuaje@gmail.com>
2023-07-07 16:34:11 -0400

    Committing the 55th exercise

---
+++ 55.firstDigit.ts
@@ -0,0 +1,16 @@
+// Output first digit of a string.
+
+function firstDigit(inputString: string): string {
+  const digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const chars = inputString.split("");
+
+  for (let i = 0; i < inputString.length; i++) {
+    if (digits.includes(inputString[i])) {
+      return inputString[i];
+    }
+  }
+}
+
+console.log(firstDigit("var_1_Int"));
+console.log(firstDigit("q2q-q"));
+console.log(firstDigit("0ss"));
