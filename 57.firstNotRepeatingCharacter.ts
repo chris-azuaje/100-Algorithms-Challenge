37ed4190b4c50a6cc0efe539f1da34a78441b105
Chris Azuaje <christopherazuaje@gmail.com>
2023-07-09 11:10:05 -0400

    Committing the 57th exercise

---
+++ 57.firstNotRepeatingCharacter.ts
@@ -0,0 +1,32 @@
+// Output first character of the string that does not repeat. If all characters repeat, return ''.
+
+function firstNotRepeatingCharacter(s: string): string {
+  const chars: string[] = s.split("");
+  let duplicates = {};
+  let answer = "_";
+  let indexAnswer = Number.MAX_SAFE_INTEGER;
+
+  chars.forEach((element, index) => {
+    if (!duplicates.hasOwnProperty(element)) {
+      duplicates[element] = {
+        count: 1,
+        index,
+      };
+    } else {
+      duplicates[element].count++;
+      duplicates[element].index = index;
+    }
+  });
+
+  for (const key in duplicates) {
+    if (duplicates[key].count === 1 && duplicates[key].index < indexAnswer) {
+      answer = key;
+      indexAnswer = duplicates[key].index;
+    }
+  }
+
+  return answer;
+}
+
+console.log(firstNotRepeatingCharacter("abacabad"));
+console.log(firstNotRepeatingCharacter("abacabaabacaba"));
