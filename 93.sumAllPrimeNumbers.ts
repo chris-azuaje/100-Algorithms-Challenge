73c512f538d6e47a04561ddec1818629a4bcf663
Chris Azuaje <christopherazuaje@gmail.com>
2023-09-26 09:44:07 -0400

    Committing the 93rd exercise

--- 93.sumAllPrimeNumbers.ts
+++ 93.sumAllPrimeNumbers.ts
@@ -1,16 +1,21 @@
 // Sum all the prime numbers up to and including the provided number. A prime number is defined as a number greater than one and having only two divisors, one and itself. For example, 2 is a prime number because it's only divisible by one and two.
 
-function sumAllPrimeNumbers(num: number): number {
-  let primeNums = [];
+function sumAllPrimeNums(num: number): number {
+  let primesTotal = 0;
+
   for (let i = 2; i < num; i++) {
-    if (num % i === 0) {
-      primeNums.push(i);
-      console.log(primeNums);
+    for (let j = 2; j <= i; j++) {
+      if (i === j) {
+        primesTotal += i;
+      }
+
+      if (i % j === 0) {
+        break;
+      }
     }
   }
+  return primesTotal;
 }
 
-// NOT FINISHED
-
-console.log(sumAllPrimeNumbers(10));
-console.log(sumAllPrimeNumbers(977));
+console.log(sumAllPrimeNums(10));
+console.log(sumAllPrimeNums(977));
